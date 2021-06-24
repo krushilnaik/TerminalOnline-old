90e04c734c7338992ecb32bfc877ab229b2f7695
Krushil Naik <krushilnaik96@gmail.com>
2021-06-23 20:16:48 -0400

    Begin working on page layout

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './scss/App.scss';
-
-function App() {
-	return <section>WORK IN PROGRESS</section>;
-}
-
-export default App;
---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './scss/App.scss';
+
+function App() {
+	return (
+		<section>
+			<div className='terminal-wrapper'>
+				<div className='terminal'>WORK IN PROGRESS</div>
+			</div>
+		</section>
+	);
+}
+
+export default App;
