cb3cb2b11c0d9afdae6744e03b389fec76e8c545
Krushil Naik <krushilnaik96@gmail.com>
2021-06-24 20:16:08 -0400

    Initialize terminal and connect to SocketIO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import { WebLinksAddon } from 'xterm-addon-web-links';
+
+import 'xterm/css/xterm.css';
 
 import './scss/App.scss';
 
 function App() {
+	useEffect(() => {
+		const socket = io('https://localhost:3000');
+		const terminal = new Terminal({
+			cursorBlink: true,
+			theme: {
+				background: 'darkslateblue'
+			}
+		});
+
+		const fitAddon = new FitAddon();
+
+		terminal.loadAddon(fitAddon);
+		terminal.loadAddon(new WebLinksAddon());
+
+		terminal.onData(data => socket.emit('input', data));
+		socket.on('output', data => terminal.write(data));
+
+		const root = document.getElementById('terminal');
+		root.innerHTML = '';
+
+		terminal.open(root);
+		fitAddon.fit();
+	});
+
 	return (
 		<section>
 			<div className='terminal-wrapper'>
@@ -10,7 +40,8 @@ function App() {
 					<button className='minimize'>-</button>
 					<button className='close'>x</button>
 				</div>
-				<div className='terminal'>TERMINAL HERE</div>
+
+				<div id='terminal'>Terminal Online</div>
 			</div>
 		</section>
 	);
